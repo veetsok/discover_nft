de6fcf12e26abac945702df5d58a7a1d3767b642
Ivan <94634131+veetsok@users.noreply.github.com>
2023-09-28 23:55:28 +0400

    Update

---
+++ src/user.InterfaceLayer/Libraries/main.library/src/UI_KIT/Widgets/BestSellers.widget/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+interface BestSellersProps {}
+
+const BestSellers: React.FC<BestSellersProps> = () => {
+  return <></>;
+};
+export default BestSellers;
--- src/user.InterfaceLayer/Pages/Main.Page/ui/MainPage.tsx
+++ src/user.InterfaceLayer/Pages/Main.Page/ui/MainPage.tsx
@@ -2,9 +2,9 @@ import RatingTable from "../../../Components/RatingTable.component.bll";
 import WeeklySlider from "../../../Components/WeeklySlider.component.bll";
 import HeroHeader from "../../../Libraries/main.library/src/UI_KIT/Cells/HeroHeader";
 import Navbar from "../../../Libraries/main.library/src/UI_KIT/Cells/Navbar";
-import Banner from "../../../Libraries/main.library/src/Widgets/banner";
+import Banner from "../../../Libraries/main.library/src/Widgets/banner.widget";
 import Marketplace from "../../../Libraries/main.library/src/Widgets/exploreMarketplace.widget";
-import Footer from "../../../Libraries/main.library/src/Widgets/footer";
+import Footer from "../../../Libraries/main.library/src/Widgets/footer.widget";
 import Unleash from "../../../Libraries/main.library/src/Widgets/unleash.widget";
 import * as ST from "../style/style";
 
